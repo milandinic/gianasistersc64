8bcdc1060a81c344edc02e14bb22bf40eb2faa36
Milan Dinic <milandinic83@gmail.com>
2014-11-17 22:52:43 +0100

    added parse

--- android/src/com/mdinic/game/giana/AndroidLauncher.java
+++ android/src/com/mdinic/game/giana/AndroidLauncher.java
@@ -4,16 +4,19 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mdinic.game.giana.GianaSistersC64;
+import com.parse.Parse;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.useAccelerometer = false;
-		config.useCompass = false;
-		config.useWakelock = true;
-		initialize(new GianaSistersC64(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useAccelerometer = false;
+        config.useCompass = false;
+        config.useWakelock = true;
+        initialize(new GianaSistersC64(), config);
+
+        Parse.initialize(this, "AKmNv40ZmUudVezreRT6NpGOBmH6D4gSOCtdnHOe", "kBM5rL0mW3zHD6ifZl9mTU5aqHPqd3Ye33k6P3CP");
+    }
 }
--- core/src/com/mdinic/game/giana/GianaSistersC64.java
+++ core/src/com/mdinic/game/giana/GianaSistersC64.java
@@ -2,10 +2,15 @@ package com.mdinic.game.giana;
 
 import com.badlogic.gdx.Game;
 import com.mdinic.game.giana.screens.GameScreen;
+import com.parse.ParseObject;
 
 public class GianaSistersC64 extends Game {
     @Override
     public void create() {
         setScreen(new GameScreen(this, 1));
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 }
