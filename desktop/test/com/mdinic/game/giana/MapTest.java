562f04fbbacc644a7d7c662ee8aebb9e2514df8c
Milan Dinic <milandinic83@gmail.com>
2015-01-16 18:09:27 +0100

    added first unit test

---
+++ desktop/test/com/mdinic/game/giana/MapTest.java
@@ -0,0 +1,29 @@
+package com.mdinic.game.giana;
+
+import org.junit.Test;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglFiles;
+import com.badlogic.gdx.backends.lwjgl.LwjglNativesLoader;
+import com.mdinic.game.giana.screens.GianaSistersScreen;
+
+public class MapTest {
+
+    @Test
+    public void testRenderLevels() {
+        GianaSistersC64 gianaSistersC64 = new GianaSistersC64();
+        gianaSistersC64.setHighScoreService(new HighScoreServiceDesktop());
+        gianaSistersC64.setSettingsService(new SettingsServiceDesktop());
+
+        LwjglNativesLoader.load();
+
+        Gdx.files = new LwjglFiles();
+        // Gdx.input = new LwjglInput();
+        // Gdx.net = new LwjglNet();
+
+        for (int i = 0; i <= GianaSistersScreen.LEVEL_COUNT; i++) {
+            Map map = new Map(i);
+        }
+
+    }
+}
