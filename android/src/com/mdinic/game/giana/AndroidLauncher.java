034f4175bbc37b0fb42d108b7744e09689c5a2cc
Milan Dinic <milan.dinic@ximedes.com>
2026-06-06 18:51:18 +0200

    feat: inject shared SupabaseHighScoreService; remove platform stubs

--- android/src/com/mdinic/game/giana/AndroidLauncher.java
+++ android/src/com/mdinic/game/giana/AndroidLauncher.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mdinic.game.giana.service.InternetConnectionChecker;
+import com.mdinic.game.giana.service.SupabaseHighScoreService;
 
 public class AndroidLauncher extends AndroidApplication implements InternetConnectionChecker {
     @Override
@@ -16,7 +17,7 @@ public class AndroidLauncher extends AndroidApplication implements InternetConne
 
         GianaSistersC64 gianaSistersC64 = new GianaSistersC64();
         gianaSistersC64.setSettingsService(new SettingsServiceDroid(this));
-        gianaSistersC64.setHighScoreService(new HighScoreServiceDroid(this, this));
+        gianaSistersC64.setHighScoreService(new SupabaseHighScoreService());
         gianaSistersC64.setGeneralService(new GeneralServiceDrod(this));
 
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
--- desktop/src/com/mdinic/game/giana/DesktopLauncher.java
+++ desktop/src/com/mdinic/game/giana/DesktopLauncher.java
@@ -2,6 +2,7 @@ package com.mdinic.game.giana;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mdinic.game.giana.service.SupabaseHighScoreService;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
@@ -9,7 +10,7 @@ public class DesktopLauncher {
         config.setTitle("Giana Byte");
         config.setWindowedMode(940, 640);
         GianaSistersC64 gianaSistersC64 = new GianaSistersC64();
-        gianaSistersC64.setHighScoreService(new HighScoreServiceDesktop());
+        gianaSistersC64.setHighScoreService(new SupabaseHighScoreService());
         gianaSistersC64.setSettingsService(new SettingsServiceDesktop());
         gianaSistersC64.setGeneralService(new GeneralServiceDesktop());
         new Lwjgl3Application(gianaSistersC64, config);
